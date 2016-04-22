dfacb395a78ea8a1ce390c4e1ef4d86f4484650c
Zura <zurab.vashakidze03@geolab.edu.ge>
2016-04-22 08:45:18 +0400

    ანდროიდის დავალება 1 (Portrait Mode)

---
+++ Homework01/app/src/main/java/android/vashakidze/zura/homework01/MainActivity.java
@@ -0,0 +1,100 @@
+package android.vashakidze.zura.homework01;
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://android.vashakidze.zura.homework01/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://android.vashakidze.zura.homework01/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    public void click(View buttons) {
+        if (buttons.getId() == R.id.red) {
+            Toast.makeText(this, "ეკრანი გაწითლდა", Toast.LENGTH_SHORT).show();
+            RelativeLayout home_page = (RelativeLayout) findViewById(R.id.home_page);
+            home_page.setBackgroundColor(Color.RED);
+        }
+        else if (buttons.getId() == R.id.blue) {
+            Toast.makeText(this, "ეკრანი გალურჯდა", Toast.LENGTH_SHORT).show();
+            RelativeLayout home_page = (RelativeLayout) findViewById(R.id.home_page);
+            home_page.setBackgroundColor(Color.BLUE);
+        }
+        else if (buttons.getId() == R.id.white) {
+            Toast.makeText(this, "ეკრანი გათეთრდა", Toast.LENGTH_SHORT).show();
+            RelativeLayout home_page = (RelativeLayout) findViewById(R.id.home_page);
+            home_page.setBackgroundColor(Color.WHITE);
+        }
+        else if (buttons.getId() == R.id.green) {
+            Toast.makeText(this, "ეკრანი გამწვანდა", Toast.LENGTH_SHORT).show();
+            RelativeLayout home_page = (RelativeLayout) findViewById(R.id.home_page);
+            home_page.setBackgroundColor(Color.GREEN);
+        }
+        else if (buttons.getId() == R.id.yellow) {
+            Toast.makeText(this, "ეკრანი გაყვითლდა", Toast.LENGTH_SHORT).show();
+            RelativeLayout home_page = (RelativeLayout) findViewById(R.id.home_page);
+            home_page.setBackgroundColor(Color.YELLOW);
+        }
+    }
+}
